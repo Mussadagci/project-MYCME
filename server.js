a562d48e82590b42d871b0ad7746e8be44ae2f6a
Mussadagci <msbagci65@gmail.comgit config --global color.ui truegit config --global core.editor emacsssh-keygen -t rsa -C msbagci65@gmail.com>
2019-11-16 14:25:51 -0800

    update repo

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-<<<<<<< Updated upstream
 const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mongoose = require("mongoose");
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
@@ -15,55 +15,12 @@ app.get("*", function(req, res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
+// connect to the Mongoose DB
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/bluedream");
+
 app.listen(PORT, function() {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
 });
 
 
-=======
-var express = require("express");
-var logger = require("morgan");
-var mongoose = require("mongoose");
-
-var PORT = 3000;
-
-// Requiring the `User` model for accessing the `users` collection
-var User = require("./userModel.js");
-
-// Initialize Express
-var app = express();
-
-// Configure middleware
-
-// Use morgan logger for logging requests
-app.use(logger("dev"));
-// Parse request body as JSON
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-// Make public a static folder
-app.use(express.static("public"));
-
-// Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/userdb", { useNewUrlParser: true });
-
-// Routes
-
-// Route to post our form submission to mongoDB via mongoose
-app.post("/submit", function(req, res) {
-  // Create a new user using req.body
-  User.create(req.body)
-    .then(function(dbUser) {
-      // If saved successfully, send the the new User document to the client
-      res.json(dbUser);
-    })
-    .catch(function(err) {
-      // If an error occurs, send the error to the client
-      res.json(err);
-    });
-});
-
-// Start the server
-app.listen(PORT, function() {
-  console.log("App running on port " + PORT + "!");
-});
->>>>>>> Stashed changes
